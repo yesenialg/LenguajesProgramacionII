f03d5f7c1edfb5f1267d3d87386c85cbec994cfb
yesenialg <50088137+yesenialg@users.noreply.github.com>
2020-03-04 20:47:03 -0500

    -

---
+++ JuegoPreg_PracticaI/app/src/main/java/com/example/juegopreg_practicai/ArchivoPlano.java
@@ -0,0 +1,65 @@
+package com.example.juegopreg_practicai;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ArchivoPlano {
+    Context cxt;
+    FileOutputStream fos;
+    FileInputStream fis;
+
+    public ArchivoPlano(Context ctx){ this.cxt = ctx; }
+
+    public ArrayList<Pregunta> Leer(){
+        try {
+            ArrayList<Pregunta> lista = new ArrayList<>();
+            int i = 2, puntaje, cont = 0;
+            String preg="", op1="", op2="", op3="", correcta="", lectura = "";
+            char caracter;
+            fis = cxt.openFileInput("Preguntas.txt");
+            while(i>0){
+                i = fis.read();
+                caracter = (char)i;
+                lectura += caracter;
+                if(i=='\n'){
+                    switch (cont){
+                        case 0:
+                            preg = lectura;
+                            break;
+                        case 1:
+                            op1 = lectura;
+                            break;
+                        case 2:
+                            op2 = lectura;
+                            break;
+                        case 3:
+                            op3 = lectura;
+                            break;
+                        case 4:
+                            correcta = lectura;
+                            break;
+                        case 5:
+                            puntaje = Integer.parseInt(lectura.trim());
+                            Pregunta p = new Pregunta (preg, op1, op2, op3, correcta, puntaje);
+                            lista.add(p);
+                            cont = -1;
+                            break;
+                    }
+                    lectura="";
+                    cont++;
+                }
+            }
+            return lista;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ JuegoPreg_PracticaI/app/src/main/java/com/example/juegopreg_practicai/Pregunta.java
@@ -0,0 +1,63 @@
+package com.example.juegopreg_practicai;
+
+public class Pregunta {
+    String pregunta, respuesta1, respuesta2, respuesta3, respuestaCorrecta;
+    int puntaje;
+
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    public String getRespuesta1() {
+        return respuesta1;
+    }
+
+    public void setRespuesta1(String respuesta1) {
+        this.respuesta1 = respuesta1;
+    }
+
+    public String getRespuesta2() {
+        return respuesta2;
+    }
+
+    public void setRespuesta2(String respuesta2) {
+        this.respuesta2 = respuesta2;
+    }
+
+    public String getRespuesta3() {
+        return respuesta3;
+    }
+
+    public void setRespuesta3(String respuesta3) {
+        this.respuesta3 = respuesta3;
+    }
+
+    public String getRespuestaCorrecta() {
+        return respuestaCorrecta;
+    }
+
+    public void setRespuestaCorrecta(String respuestaCorrecta) {
+        this.respuestaCorrecta = respuestaCorrecta;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public Pregunta(String pregunta, String respuesta1, String respuesta2, String respuesta3, String respuestaCorrecta, int puntaje){
+        this.pregunta = pregunta;
+        this.respuesta1 = respuesta1;
+        this.respuesta2 = respuesta2;
+        this.respuesta3 = respuesta3;
+        this.respuestaCorrecta = respuestaCorrecta;
+        this.puntaje = puntaje;
+    }
+}
